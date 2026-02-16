fb7e5d8f1aff22ee12b6172db7a92f729d51df75
Mykayla0331 <lewism1735@student.faytechcc.edu>
2026-02-16 15:47:04 +0000

    working on m3t2

--- m3t2.cpp
+++ m3t2.cpp
@@ -3,3 +3,24 @@
 // MYKAYLA LEWIS
 // 2/16/26
 // START THE GAME OF CRAPS
+
+#include <iostream>
+using namespace std;
+
+// Function declarations
+// (tell the program what the functions will be)
+int roll();
+
+// main() goes here
+int main () {
+    int number = roll();
+    cout << "You rolled a " << number << endl;
+    return 0;
+}
+
+// fuction definitions 
+// (actually write the entire function)
+
+int roll() {
+    return 1;
+}
